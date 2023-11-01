43260473be422c962ac7614ad8cc9d1e4fd54d9a
Dmitry <gunin.syntax@gmail.com>
2023-11-01 21:27:02 +0400

    Correction of the task to the conditions

--- src/main/java/main_tasks/SimpleNumbers.java
+++ src/main/java/main_tasks/SimpleNumbers.java
@@ -1,15 +1,23 @@
 package main_tasks;
 
 public class SimpleNumbers {
-    public static void main(String[] args) {
-        for (int i = 2; i < 100; i++) {
-            for (int j = 2; j <= i; j++) {
-                if (i % j == 0 && j < i) {
-                    break;
-                } else if (i % j == 0) {
-                    System.out.print(i + " ");
-                }
+
+    static void simpleNumber(int number) {
+        if (number == 1) {
+            System.out.println(number + " не является простым или составным числом");
+        }
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0 && i < number) {
+                System.out.println(number + " это не простое число, а составное");
+                return;
+            } else if (number % i == 0) {
+                System.out.println(number + " это простое число");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int number = 11;
+        simpleNumber(number);
     }
 }
